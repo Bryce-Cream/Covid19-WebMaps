5c3538da7c399de43dc9b48bd665e862529f920d
Bryce Hughson <hughsonb@uwindsor.ca>
2020-05-16 22:28:20 -0400

    Updated covidMap.py
    Had a weird bug where I couldn't upload this commit with the rest of them

--- covidMap.py
+++ covidMap.py
@@ -2,7 +2,9 @@ import folium
 import pandas
 import requests
 #Creating the base map to start at the University Windsor
-map = folium.Map(location=[42.30, -83.06], zoom_start=6, tiles="Stamen Terrain")
+new_cases_map = folium.Map(location=[42.30, -83.06], zoom_start=6, tiles="Stamen Terrain")
+new_deaths_map = folium.Map(location=[42.30, -83.06], zoom_start=6, tiles="Stamen Terrain")
+active_cases_map = folium.Map(location=[42.30, -83.06], zoom_start=6, tiles="Stamen Terrain")
 
 #I am using API from thevirustracker.com
 res = requests.get('https://api.thevirustracker.com/free-api?countryTotals=ALL')
@@ -21,6 +23,15 @@ for x in range(1, len(covid_current['countryitems'][0])):
 
 df_covid = pandas.DataFrame(df, columns = ['Country', 'New Cases Today','New Deaths Today', 'Total Active Cases'])
 
+#Getting the maxes for each of the categories
+maxNCT = df_covid['New Cases Today'].max() + 1
+maxNDT = df_covid['New Deaths Today'].max() + 1
+maxTAC = df_covid['Total Active Cases'].max() + 1
+
+#Data for the polygons of countries
+url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
+country_shapes = f'{url}/world-countries.json'
+
 #Replacing the country names to from the dataset to match Foliums provided
 df_covid.replace('USA', "United States of America", inplace = True)
 df_covid.replace('Tanzania', "United Republic of Tanzania", inplace = True)
@@ -32,72 +43,46 @@ df_covid.replace('Serbia', "Republic of Serbia", inplace = True)
 df_covid.replace('Czechia', "Czech Republic", inplace = True)
 df_covid.replace('UAE', "United Arab Emirates", inplace = True)
 
-
-
-
-
-
-
-
-
-
-"""
-#Adding the Choropleth layer onto our base map
 folium.Choropleth(
-    #The GeoJSON data to represent the world country
     geo_data=country_shapes,
-    name='choropleth COVID-19',
+    name='New Cases Today',
     data=df_covid,
-    #The column aceppting list with 2 value; The country name and  the numerical value
-    columns=['Country', 'Total Case'],
+    columns=['Country', 'New Cases Today'],
     key_on='feature.properties.name',
-    fill_color='PuRd',
-    nan_fill_color='white'
-).add_to(m)
+    bins=[0, maxNCT*.2, maxNCT*.4, maxNCT*.6, maxNCT*.8, maxNCT],
+    nan_fill_color='white',
+    fillColor='YlOrRd',
+    highlight=True
+).add_to(new_cases_map)
 
+folium.Choropleth(
+    geo_data=country_shapes,
+    name='New Deaths Today',
+    data=df_covid,
+    columns=['Country', 'New Deaths Today'],
+    key_on='feature.properties.name',
+    fillColor='YlOrRd',
+    bins=[0, maxNDT*.2, maxNDT*.4, maxNDT*.6, maxNDT*.8, maxNDT],
+    nan_fill_color='white',
+    highlight=True
+).add_to(new_deaths_map)
 
+folium.Choropleth(
+    geo_data=country_shapes,
+    name='Active Cases',
+    data=df_covid,
+    columns=['Country', 'Total Active Cases'],
+    key_on='feature.properties.name',
+    fillColor='YlOrRd',
+    bins=[0, maxTAC*.2, maxTAC*0.4, maxTAC*.6, maxTAC*0.8, maxTAC],
+    nan_fill_color='white',
+    highlight=True
+).add_to(active_cases_map)
 
+folium.LayerControl().add_to(new_cases_map)
+folium.LayerControl().add_to(new_deaths_map)
+folium.LayerControl().add_to(active_cases_map)
 
-for lat, lon, name in   zip(country['latitude'],country['longitude'],country['name']):
-    #Creating the marker
-    folium.Marker(
-    #Coordinate of the country
-    location=[lat, lon],
-    #The popup that show up if click the marker
-    popup=name
-    ).add_to(m)
-m
-"""
-
-data = pandas.read_csv("volcanoes.txt")
-lat = list(data["LAT"])
-lon = list(data["LON"])
-elev = list(data["ELEV"])
-
-def color_producer(elevation):
-    if elevation < 1000:
-        return 'green'
-    elif 1000<= elevation < 3000:
-        return 'orange'
-    else:
-        return 'red'
-
-
-fgv = folium.FeatureGroup(name="Volcanoes")
-
-#Used for iterating through two list simultaneously
-for lt, ln, el in zip(lat, lon, elev):
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius= 6, popup=str(el)+" m",
-    fill_color=color_producer(el), color='grey', fill_opacity=0.7))#May need to put fill = True if error
-
-fgp = folium.FeatureGroup(name="Population")
-
-fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
-style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
-else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
-
-map.add_child(fgv)
-map.add_child(fgp)
-map.add_child(folium.LayerControl())
-
-map.save("Map1.html")
+new_cases_map.save("NewCasesMap.html")
+new_deaths_map.save("NewDeathsMap.html")
+active_cases_map.save("ActiveCasesMap.html")
